d0a5a073cf9901c41cf41fce6e85a24f03234900
leafcabral <rafaelcabral4100@gmail.com>
2026-09-01 10:24:58 -0300

    aula

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,6 +4,7 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+$routes->get('/vitrine', '');
 $routes->get('/rota-legal', 'RotaLegal::index');
 $routes->get('/rota-do-mal', 'RodaDoMal::index');
 $routes->get('/rota-2', 'Rota2::index');
