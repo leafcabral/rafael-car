6537a2e6e18835b6bffd01082f4b8cc8326ff1ae
leafcabral <rafaelcabral4100@gmail.com>
2026-09-02 09:02:38 -0300

    coisa

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,8 +3,8 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
-$routes->get('/vitrine', '');
+$routes->get('/', 'CarroController::index');
+// $routes->get('/vitrine', 'CarroController::index');
 $routes->get('/rota-legal', 'RotaLegal::index');
 $routes->get('/rota-do-mal', 'RodaDoMal::index');
 $routes->get('/rota-2', 'Rota2::index');
